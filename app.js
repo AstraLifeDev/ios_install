cf95750af726e9a6d98c21fc228c4e8f5fa25e24
T0V4 <47669048+t0v4@users.noreply.github.com>
2026-06-18 22:43:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const DEFAULT_METADATA = {
   version: "0.0.7",
   subtitle: "Мобильная версия лаунчера AstraLife для устройств iOS и iPadOS.",
   website: "https://astralife.top",
-  updatedAt: "16 июня 2026",
-  size: "10 МБ"
+  updatedAt: "18 июня 2026",
+  size: "5 МБ"
 };
 
 const STORAGE_KEY = "astraMobileInstallPhase";
